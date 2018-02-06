7ead65013ed2267f2cc794eca6809f562fad9da2
marouane <elkassemi@outlook.com>
2018-02-06 01:02:32 +0000

    working on file upload

--- src/app/pages/import/import.ts
+++ src/app/pages/import/import.ts
@@ -6,8 +6,10 @@ import * as moment from "moment"
 import { locale } from 'moment';
 import { LocalDataSource } from 'ng2-smart-table';
 import { FileUploader } from 'ng2-file-upload';
+import { PerimetreService } from '../../services/perimetre.service'
+import {ProjetService} from '../../services/projet.service';
 
-const URL = 'http://localhost:8080/upload';
+const URL = 'http://localhost:8080/api/upload';
 
 
 @Component({
@@ -18,12 +20,19 @@ const URL = 'http://localhost:8080/upload';
 
 
 export class ImportPage implements OnInit {
-    constructor(){}
-    uploader: FileUploader = new FileUploader({url: URL}); //Empty options to avoid having a target URL
+    constructor(
+        private perimetreservice:PerimetreService,
+    ){}
     reader: FileReader = new FileReader();
-       
-    source : LocalDataSource
-    loading : boolean = true 
+    msgs : any = [];
+    cName : String;
+    selectedc;
+    collection;
+    collectionList;
+    SupportList
+    source : LocalDataSource;
+    loading : boolean = true;
+    uploader: FileUploader = new FileUploader({url: URL}); //Empty options to avoid having a target URL
 
     settings = {
         columns: {
@@ -38,12 +47,56 @@ export class ImportPage implements OnInit {
             perPage:25
         },
         noDataMessage:' '
-    }
+    };
 
-    
+    createCollection(name,type){
+        this.perimetreservice.createCollection({name:name,type:type}).then((data) =>{
+            this.getCollection()
+        })
+    }
+    getCollection(){
+        this.perimetreservice.getCollection().then((data) => {
+            this.collectionList = data
+        } );
+    }
+    FileSelected(name) {
+        this.uploader.onBeforeUploadItem = (item) => {
+            item.withCredentials = false;
+            item.url = URL + '?id=' + name._id;
+        }
+    }
+    upload(name){
+        // this.uploader.options.additionalParameter = {
+        //     parent_id: name._id
+        // };
+        this.uploader.options.url = URL + '?id=' + name._id;
+        this.uploader.uploadAll()
+    }
+    getSupport(id){
+        this.perimetreservice.getSupport(id).then((data : any) =>{
+            this.SupportList = data
+            let results = []
+            console.log(data)
+            data.forEach(element => {
+                Object.keys(element.properties).forEach(p =>{
+                    this.settings.columns[p] = {'title':p}
+                })
+                results.push(element.properties)
+            });
+            this.source = new LocalDataSource(results);
+            this.loading = false
+        } )
+    }
     ngOnInit(){
         this.uploader.onAfterAddingFile = (item => {
             item.withCredentials = false;
          });
+        this.getCollection();
+
+        this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+            let responsePath = JSON.parse(response);
+            this.msgs.push({severity:'success', summary:'', detail:'Nombre de collectes correspondant à vos critères de recherche : '+ responsePath.inserted });
+            console.log(response, responsePath);// the url will be in the response
+        };
     }
 }
--- src/app/services/perimetre.service.ts
+++ src/app/services/perimetre.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from './Http-client';
 import 'rxjs/add/operator/map';
+import {Response} from '@angular/http';
 
 @Injectable()
 export class PerimetreService {
@@ -58,6 +59,44 @@ export class PerimetreService {
     getDpaOffice(){
         return new Promise((resolve, reject) => {
             this.http.get('perimetre/DpaOffice/')
+                .map(res => res.json())
+                .subscribe(data => {
+                    resolve(data);
+                }, (err) => {
+                    reject(err);
+                });
+        });
+    }
+    getCollection(){
+        return new Promise((resolve, reject) => {
+            this.http.get('perimetre/collections')
+                .map(res => res.json())
+                .subscribe(data => {
+                    resolve(data);
+                }, (err) => {
+                    reject(err);
+                });
+        });
+    }
+
+    createCollection(data){
+        return new Promise((resolve, reject) => {
+
+            this.http.post('perimetre/collections', JSON.stringify(data),)
+                .map(res => res.json())
+                .subscribe(res => {
+                    resolve(res);
+                },(err:Response) => {
+                    let details = err.json();
+                    reject(details);
+                });
+
+        });
+    }
+
+    getSupport(id){
+        return new Promise((resolve, reject) => {
+            this.http.get('perimetre/support?id='+id)
                 .map(res => res.json())
                 .subscribe(data => {
                     resolve(data);
